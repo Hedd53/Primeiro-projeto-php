c0dfbfcc80b86970de2c31b481363ac78d91bbcd
Heduardo Veridiano Reis <99209281+Hedd53@users.noreply.github.com>
2023-02-27 16:46:37 -0300

    Add files via upload

---
+++ PrimeiroProjeto/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projetin</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Hello mundo</h1>";
+
+        echo "<h2>", (5 + 3), "</h2>";
+        echo "<h2>" , (103 - 7),"</h2>";
+        echo "<h2>" , (5 * 3 + 6),"</h2>";
+        echo "<h2>" , (5 / 3),"</h2>";
+
+    ?>
+
+</body>
+</html>
